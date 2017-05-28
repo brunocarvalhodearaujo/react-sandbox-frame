ac59574416fe1a68eef542f481a4c02fb4f4695d
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2017-05-28 14:32:25 -0300

    fix

--- src/Frame.js
+++ src/Frame.js
@@ -8,28 +8,51 @@ import { pickBy } from 'lodash'
 
 export default class Frame extends Component {
 
+  get DOMNode () {
+    return (findDOMNode(this).contentDocument || findDOMNode(this).contentWindow.document)
+  }
+
   /**
-   *
-   * @param {HTMLElement} DOMNode
+   * @return {Promise}
    */
-  renderFrame (DOMNode = (findDOMNode(this).contentDocument || findDOMNode(this).contentWindow.document)) {
-    if (DOMNode.readyState === 'complete') {
+  isReady () {
+    return new Promise((resolve, reject) => {
+      try {
+        const DOMNode = this.DOMNode
+        if (DOMNode.readyState === 'complete') {
+          resolve(true)
+        }
+        DOMNode.onload = () => {
+          resolve(true)
+        }
+      } catch (error) {
+        resolve(false)
+      }
+    })
+  }
+
+  async renderFrame () {
+    const ready = await this.isReady()
+    if (ready) {
+      const DOMNode = this.DOMNode
       if (this.props.hasOwnProperty('children') && !this.props.hasOwnProperty('src')) {
         DOMNode.body.innerHTML = is.string(this.props.children)
-        ? this.props.children
-        : renderToStaticMarkup(this.props.children)
+          ? this.props.children
+          : renderToStaticMarkup(this.props.children)
       }
-      const head = DOMNode.getElementsByTagName('head')[0]
-      this.props.stylesheets.forEach(url => {
-        if (!head.querySelector(`link[href="${url}"]`)) {
-          const ref = DOMNode.createElement('link')
-          ref.rel = 'stylesheet'
-          ref.type = 'text/css'
-          ref.href = url
-          head.appendChild(ref)
-        }
-      })
-      if (!head.querySelector(`script[of="${this.props.title}"]`) && this.props.scripts.length) {
+      const head = DOMNode.getElementsByTagName('head')[ 0 ]
+      if (this.props.hasOwnProperty('stylesheets')) {
+        this.props.stylesheets.forEach(url => {
+          if (!head.querySelector(`link[href="${url}"]`)) {
+            const ref = DOMNode.createElement('link')
+            ref.rel = 'stylesheet'
+            ref.type = 'text/css'
+            ref.href = url
+            head.appendChild(ref)
+          }
+        })
+      }
+      if (!head.querySelector(`script[of="${this.props.title}"]`)) {
         var $script = fs.readFileSync('./node_modules/scriptjs/dist/script.min.js', 'utf8')
         const scriptElement = DOMNode.createElement('script')
         scriptElement.setAttribute('type', 'text/javascript')
